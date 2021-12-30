23e70ac44cb541dde1f0011b7cbef6ab62bb3691
Edering <matejdurana0@gmail.com>
2021-12-30 16:35:26 +0100

    animovany lordicon

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,24 @@
     <v-main v-if="!isLoading" class="content">
       <Header></Header>
       <router-view />
+
+      <!-- <lottie :options="defaultOptions" :height="65" :width="65" /> -->
+      <lottie :options="defaultOptions" :height="400" :width="400" v-on:animCreated="handleAnimation"/>
+      <div>
+        <p>Speed: x{{ animationSpeed }}</p>
+        <input
+          type="range"
+          value="1"
+          min="0"
+          max="3"
+          step="0.5"
+          v-on:change="onSpeedChange"
+          v-model="animationSpeed"
+        />
+      </div>
+      <button v-on:click="stop">stop</button>
+      <button v-on:click="pause">pause</button>
+      <button v-on:click="play">play</button>
     </v-main>
   </v-app>
 </template>
@@ -13,22 +31,48 @@
 import LoadingScreen from "@/components/LoadingScreen.vue";
 import Header from "@/components/Header.vue";
 import ParticlesJS from "@/components/ParticlesJS.vue";
+import Lottie from "vue-lottie";
+import animationData from "@/assets/394-scroll-down-6-auto-edited.json";
 
 export default {
   components: {
     LoadingScreen,
     Header,
     ParticlesJS,
+    Lottie,
   },
   data() {
     return {
       isLoading: true,
+      defaultOptions: { animationData: animationData },
+      animationSpeed: 1,
     };
   },
   mounted() {
     setTimeout(() => {
       this.isLoading = false;
     }, 1500);
+  },
+  methods: {
+    handleAnimation: function (anim) {
+      this.anim = anim;
+    },
+
+    stop: function () {
+      this.anim.stop();
+    },
+
+    play: function () {
+      this.anim.play();
+    },
+
+    pause: function () {
+      this.anim.pause();
+    },
+
+    onSpeedChange: function () {
+      this.anim.setSpeed(this.animationSpeed);
+    },
   },
 };
 </script>
